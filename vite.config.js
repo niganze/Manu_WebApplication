1f64fede90393f38b00c6da23574970132deeace
niganze <niganzealain@gmail.com>
2025-03-10 16:53:17 +0200

    Customized user-dashboard

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    historyApiFallback: true, // Ensures React Router works on refresh
+  },
+  build: {
+    outDir: 'dist', // Output directory for production build
+  },
+
 })
