3c689634bf341ad67e419a185e255619edc74253
muhire <dushimiyimanaelisa@gmail.com>
2025-04-03 11:31:26 +0200

    fix some change

--- src/Protection.jsx
+++ src/Protection.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 
 function Protection({ children }) {
-  const userToken = localStorage.getItem("userToken"); // Retrieve token from localStorage
-  const accessToken = userToken?.user?.tokens?.accessToken; 
+  const userTokenString = localStorage.getItem("userToken"); // Retrieves string from localStorage
+  const userToken = userTokenString ? JSON.parse(userTokenString) : null; // Parse the string to an object
+  const accessToken = userToken?.user?.tokens?.accessToken;
   const isAuthenticated = !!accessToken; // Check if user is authenticated (token exists)
+  const userRole = userToken?.user?.role;
+  console.log("===========", userRole);
+  const navigate = useNavigate();
 
-  if (!isAuthenticated) {
+  if (userRole === "Admin" && isAuthenticated) {
+    navigate("/admindashboard");
+  } else if (userRole === "user" && isAuthenticated) {
+    navigate("/user-dashboard");
+  } else {
     // If user is not authenticated, redirect to login page
     return <Navigate to="/login" replace />;
   }
